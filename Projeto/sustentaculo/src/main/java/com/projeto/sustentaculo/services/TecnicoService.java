5c79efebcd49bccc6dbc88b529b141c9676d24c0
Alan Santos <ss.alan@outlook.com>
2022-09-28 15:10:16 -0300

    update

--- Projeto/sustentaculo/src/main/java/com/projeto/sustentaculo/services/TecnicoService.java
+++ Projeto/sustentaculo/src/main/java/com/projeto/sustentaculo/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.projeto.sustentaculo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,16 +41,24 @@ public class TecnicoService {
 		return repository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
+
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("CPF já cadastrado!");
 		}
-		
+
 		obj = pessoaRepository.findByEmail(objDTO.getEmail());
 		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("Email já cadastrado!");
 		}
 	}
-}
 
+}
