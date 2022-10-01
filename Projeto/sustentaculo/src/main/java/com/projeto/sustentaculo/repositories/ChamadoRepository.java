80df6d4c58b666d5dc7757219fceb801aa9af287
Alan Santos <ss.alan@outlook.com>
2022-10-01 00:26:16 -0300

    apagando pastas

--- Projeto/sustentaculo/src/main/java/com/projeto/sustentaculo/repositories/ChamadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.sustentaculo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.sustentaculo.domain.Chamado;
-
-public interface ChamadoRepository extends JpaRepository<Chamado, Integer> {
-
-}
--- Projeto/sustentaculo/src/main/java/com/projeto/sustentaculo/repositories/TecnicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.sustentaculo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.sustentaculo.domain.Tecnico;
-
-public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
-
-}
