29a99db3dd0de3eadcd84ca9b5e0e8e1be2ac0e1
Alan Santos <ss.alan@outlook.com>
2022-10-03 16:19:06 -0300

    codificacao ao atualizar técnico

--- src/main/java/com/projeto/sustentaculo/services/TecnicoService.java
+++ src/main/java/com/projeto/sustentaculo/services/TecnicoService.java
@@ -47,6 +47,7 @@ public class TecnicoService {
     public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
         objDTO.setId(id);
         Tecnico oldObj = findById(id);
+        objDTO.setSenha(encoder.encode(objDTO.getSenha()));
         validaPorCpfEEmail(objDTO);
         oldObj = new Tecnico(objDTO);
         return repository.save(oldObj);
